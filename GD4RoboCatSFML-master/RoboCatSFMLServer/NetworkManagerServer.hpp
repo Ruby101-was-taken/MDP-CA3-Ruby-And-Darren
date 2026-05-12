f4948d80dcb45073ebdfcd47ff43c6e90c5ef7cd
Darren <D00255479@student.dkit.ie>
2026-05-12 18:55:45 +0100

    🎨 Replace tabs with spaces for variables

--- GD4RoboCatSFML-master/RoboCatSFMLServer/NetworkManagerServer.hpp
+++ GD4RoboCatSFML-master/RoboCatSFMLServer/NetworkManagerServer.hpp
@@ -3,57 +3,62 @@ class NetworkManagerServer : public NetworkManager
 public:
 	static NetworkManagerServer* sInstance;
 
-	static bool				StaticInit(uint16_t inPort);
+	static bool	StaticInit(uint16_t inPort);
 
-	virtual void			ProcessPacket(InputMemoryBitStream& inInputStream, const SocketAddress& inFromAddress) override;
-	virtual void			HandleConnectionReset(const SocketAddress& inFromAddress) override;
+	virtual void ProcessPacket(InputMemoryBitStream& inInputStream, const SocketAddress& inFromAddress) override;
+	virtual void HandleConnectionReset(const SocketAddress& inFromAddress) override;
 
-	void			SendOutgoingPackets();
-	void			CheckForDisconnects();
+	// Lobby / game state control:
+	// When true, server accepts new joining players (lobby). When false, reject new joins (game in progress).
+	void SetIsInLobby(bool inIsInLobby) { mIsInLobby = inIsInLobby; }
+	bool IsInLobby() const { return mIsInLobby; }
 
-	void			RegisterGameObject(GameObjectPtr inGameObject);
-	inline	GameObjectPtr	RegisterAndReturn(GameObject* inGameObject);
-	void			UnregisterGameObject(GameObject* inGameObject);
-	void			SetStateDirty(int inNetworkId, uint32_t inDirtyState);
+	void SendOutgoingPackets();
+	void CheckForDisconnects();
 
-	void			RespawnCats();
+	void RegisterGameObject(GameObjectPtr inGameObject);
+	inline GameObjectPtr RegisterAndReturn(GameObject* inGameObject);
+	void UnregisterGameObject(GameObject* inGameObject);
+	void SetStateDirty(int inNetworkId, uint32_t inDirtyState);
 
-	ClientProxyPtr	GetClientProxy(int inPlayerId) const;
+	void RespawnCats();
 
-	void	UpdateAllClients();
+	ClientProxyPtr GetClientProxy(int inPlayerId) const;
+
+	void UpdateAllClients();
 private:
 	NetworkManagerServer();
 
-	void	HandlePacketFromNewClient(InputMemoryBitStream& inInputStream, const SocketAddress& inFromAddress);
-	void	ProcessPacket(ClientProxyPtr inClientProxy, InputMemoryBitStream& inInputStream);
+	void HandlePacketFromNewClient(InputMemoryBitStream& inInputStream, const SocketAddress& inFromAddress);
+	void ProcessPacket(ClientProxyPtr inClientProxy, InputMemoryBitStream& inInputStream);
 
-	void	SendWelcomePacket(ClientProxyPtr inClientProxy);
-	void	HandleClientDisconnected(ClientProxyPtr inClientProxy);
+	void SendWelcomePacket(ClientProxyPtr inClientProxy);
+	void HandleClientDisconnected(ClientProxyPtr inClientProxy);
 
-	void	AddWorldStateToPacket(OutputMemoryBitStream& inOutputStream);
-	void	AddScoreBoardStateToPacket(OutputMemoryBitStream& inOutputStream);
+	void AddWorldStateToPacket(OutputMemoryBitStream& inOutputStream);
+	void AddScoreBoardStateToPacket(OutputMemoryBitStream& inOutputStream);
 
-	void	SendStatePacketToClient(ClientProxyPtr inClientProxy);
-	void	WriteLastMoveTimestampIfDirty(OutputMemoryBitStream& inOutputStream, ClientProxyPtr inClientProxy);
+	void SendStatePacketToClient(ClientProxyPtr inClientProxy);
+	void WriteLastMoveTimestampIfDirty(OutputMemoryBitStream& inOutputStream, ClientProxyPtr inClientProxy);
 
-	void	HandleInputPacket(ClientProxyPtr inClientProxy, InputMemoryBitStream& inInputStream);
+	void HandleInputPacket(ClientProxyPtr inClientProxy, InputMemoryBitStream& inInputStream);
 
-	
-
-	int		GetNewNetworkId();
+	int GetNewNetworkId();
 
 	typedef unordered_map< int, ClientProxyPtr >	IntToClientMap;
 	typedef unordered_map< SocketAddress, ClientProxyPtr >	AddressToClientMap;
 
-	AddressToClientMap		mAddressToClientMap;
-	IntToClientMap			mPlayerIdToClientMap;
+	AddressToClientMap mAddressToClientMap;
+	IntToClientMap mPlayerIdToClientMap;
 
-	int				mNewPlayerId;
-	int				mNewNetworkId;
+	int mNewPlayerId;
+	int mNewNetworkId;
 
-	float			mTimeOfLastSatePacket;
-	float			mTimeBetweenStatePackets;
-	float			mClientDisconnectTimeout;
+	float mTimeOfLastSatePacket;
+	float mTimeBetweenStatePackets;
+	float mClientDisconnectTimeout;
+	// true by default - server starts in lobby allowing joins
+	bool mIsInLobby;
 };
 
 
